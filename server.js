e246ea053124266b02f9b3848ffaa4b7916c127d
Matt Mannion <mattpmannion@gmail.com>
2020-12-27 17:37:21 -0500

    added more js

--- server.js
+++ server.js
@@ -1,9 +1,57 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    //.connect(process.env.DATABASE_LOCAL, {and fit the stuff below in here})
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Park Camper',
+    price: 997,
+});
+
+testTour
+    .save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((err) => {
+        console.log('error!!!!!!: ', err);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log(`App running on port ${port}...`);
 });
