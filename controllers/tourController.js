95b2fdf8fafd6af2f9d4ef35ca608ec984066ab0
Matt Mannion <mattpmannion@gmail.com>
2020-12-27 19:43:43 -0500

    added more js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,57 +1,32 @@
-/* eslint-disable prefer-object-spread */
-const fs = require('fs');
-
-const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
-
-exports.checkID = (req, res, next, val) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        });
-    }
-    next();
-};
-
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price',
-        });
-    }
-    next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours,
-        },
+        // results: tours.length,
+        // data: {
+        //     tours,
+        // },
     });
 };
 
-exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
 
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        () => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour,
-                },
-            });
-        }
-    );
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: 'Invalid data sent!',
+        });
+    }
 };
 
 exports.getTour = (req, res) => {
@@ -60,10 +35,10 @@ exports.getTour = (req, res) => {
 
     res.status(200).json({
         status: 'success',
-        results: tours.length,
-        data: {
-            tour,
-        },
+        // results: tours.length,
+        // data: {
+        //     tour,
+        // },
     });
 };
 
