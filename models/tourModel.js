a6664bf8cb24d6c5cf17681a21c7d800c3ca61e2
Matt Mannion <mattpmannion@gmail.com>
2020-12-28 19:34:50 -0500

    added more js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -29,6 +30,10 @@ const tourSchema = new mongoose.Schema(
         difficulty: {
             type: String,
             required: [true, 'A tour must have a difficulty'],
+            enum: {
+                values: ['easy', 'medium', 'difficult'],
+                message: 'Difficulty is either: easy, meduim, or difficult',
+            },
         },
         ratingsAverage: {
             type: Number,
@@ -44,7 +49,17 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             required: [true, 'A tour must have a price'],
         },
-        priceDiscount: Number,
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    // this only points to the current doc on the NEW document creation
+                    return val < this.price;
+                },
+                message:
+                    'Discount price ({VALUE}) should be below the regular price',
+            },
+        },
         summary: {
             type: String,
             trim: true,
