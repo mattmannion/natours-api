bf98aa58530ad38762870cf9b847a3538d459d26
Matt Mannion <mattpmannion@gmail.com>
2020-12-30 12:48:30 -0500

    added more js

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,3 +51,27 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1 - getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token)
+    return next(
+      new AppError('You are not logged in! Please login to get access.', 401)
+    );
+  // 2 - verification token
+
+  // 3 - check if user still exists
+
+  // 4 - check if user changed passwords after the token was issued
+
+  next();
+});
