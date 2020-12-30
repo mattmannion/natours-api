d0ee297d66d495607bc1b59a795a7b4bb9094edb
Matt Mannion <mattpmannion@gmail.com>
2020-12-30 14:17:00 -0500

    added more js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
@@ -61,17 +62,29 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token);
 
   if (!token)
     return next(
       new AppError('You are not logged in! Please login to get access.', 401)
     );
+
   // 2 - verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3 - check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser)
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    );
 
   // 4 - check if user changed passwords after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat))
+    return next(
+      new AppError('User recently changed password! Please login again.', 401)
+    );
 
+  // grant access to protected route
+  req.user = currentUser;
   next();
 });
