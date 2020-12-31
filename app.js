88eac0d853b82a89e4c027265c5129b4847e0a3c
Matt Mannion <mattpmannion@gmail.com>
2020-12-31 17:38:15 -0500

    added more js

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const { use } = require('./routes/tourRoutes');
 
 const app = express();
 ///////////////////////////////////////
 //// Middleware ///////////////////////
 /////////////////////////////////////
+
+// security http headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.use(express.json());
+// limit requests from same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against nosql query injection
+app.use(mongoSanitize());
+
+// data sanitization against xss
+app.use(xss());
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// test middleware
 app.use((req, res, next) => {
   // console.log('Something happened on the Server!');
   next();
--- controllers/authController.js
+++ controllers/authController.js
@@ -21,8 +21,13 @@ const createSendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
   };
+
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
   res.cookie('jwt', token, cookieOptions);
+
+  // remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
